c0ae6b6065f032ea4726ec0359e6706985093317
Shyr-a <Sot-2004@mail.ru>
2022-06-17 10:40:51 +0500

    first_part

--- app/src/main/java/com/example/trygit/MainActivity.kt
+++ app/src/main/java/com/example/trygit/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.trygit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton:Button
+    private lateinit var fasleButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.tru)
+        fasleButton = findViewById(R.id.fals)
+        trueButton.setOnClickListener{ view: View ->
+        Toast.makeText(
+            this,
+            R.string.correct_toast,
+            Toast.LENGTH_SHORT).show()
+
+
+        }
+        fasleButton.setOnClickListener { view: View ->
+        Toast.makeText(
+
+            this,
+            R.string.incorrect_toast,
+            Toast.LENGTH_SHORT).show()
+
+
+
+        }
     }
 }
