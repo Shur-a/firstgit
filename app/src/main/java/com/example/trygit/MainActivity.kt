34eb7c21b5ed0e819b6a0b4376f1cab1df9b8845
Shyr-a <Sot-2004@mail.ru>
2022-06-22 13:05:42 +0500

    third_part

--- app/src/main/java/com/example/trygit/MainActivity.kt
+++ app/src/main/java/com/example/trygit/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton:Button
     private lateinit var fasleButton:Button
-    private lateinit var  nextButton:Button
+    private lateinit var  nextButton:ImageButton
+    private lateinit var  prevButton:ImageButton
     private  lateinit var  questionTextView: TextView
     private val questionBank= listOf(
         Questions(R.string.text1,true),
@@ -27,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.tru)
         fasleButton = findViewById(R.id.fals)
         nextButton=findViewById(R.id.next_button)
+        prevButton=findViewById(R.id.prev_button)
+
         questionTextView=findViewById((R.id.question_text_view))
         trueButton.setOnClickListener{ view: View ->
         Toast.makeText(
@@ -49,6 +53,14 @@ class MainActivity : AppCompatActivity() {
         questionTextView.setText(questionTextResId)
         nextButton.setOnClickListener {
             currentIndex=(currentIndex + 1)% questionBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener {
+            currentIndex = if (currentIndex>0){
+                (currentIndex - 1)
+            } else {
+                questionBank.size-1
+            }
             updateQuestion()
         }
     }
